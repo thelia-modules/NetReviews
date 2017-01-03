667523950d888e51360b8d0e464d3bd34ddd2392
Vincent Lopes <vlopes@openstudio.fr>
2017-01-03 11:16:34 +0100

    Add french translation

---
+++ I18n/backOffice/default/fr_FR.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'Code de l\'iframe produit :' => 'Code de l\'iframe produit :',
+    'Code du widget site :' => 'Code du widget site :',
+    'NetReviews configuration' => 'Configuration d\'Avis vérifiés ',
+    'NetReviews module configuration' => 'Configuration du module Avis vérifiés',
+    'Send' => 'Envoyer',
+    'Send order to NetReviews' => 'Envoyer la commande a Avis Vérifiés',
+);
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,4 +1,13 @@
 <?php
+
 return array(
-    // 'an english string' => 'La traduction française de la chaine',
+    'Error' => 'Erreur',
+    'Footer link' => 'Lien du pied de page',
+    'Footer link title' => 'Texte du lien dans le pied de page',
+    'Id website' => 'Identifiant Site Web',
+    'Net Reviews' => 'Avis vérifiés',
+    'Platform language' => 'Langue de la plateforme',
+    'Product iframe code' => 'Code de l\'iframe produit',
+    'Secret token' => 'Votre Clé Secrète',
+    'Site widget code' => 'Code du widget site',
 );
