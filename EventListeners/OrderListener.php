3f9069508a0717a22fe76c700d1e864810472aef
Nicolas Barbey <223106@supinfo.com>
2021-02-16 10:22:34 +0100

    Add filter order by status for OrderListener

--- EventListeners/OrderListener.php
+++ EventListeners/OrderListener.php
@@ -4,6 +4,7 @@
 namespace NetReviews\EventListeners;
 
 use NetReviews\Model\NetreviewsOrderQueue;
+use NetReviews\NetReviews;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Thelia\Core\Event\Order\OrderEvent;
 use Thelia\Core\Event\TheliaEvents;
@@ -19,9 +20,12 @@ class OrderListener implements EventSubscriberInterface
 
     public function registerOrder(OrderEvent $event)
     {
-        $netReviewsOrderQueue = new NetreviewsOrderQueue();
-        $netReviewsOrderQueue->setOrderId($event->getPlacedOrder()->getId())
-            ->setStatus(0)
-            ->save();
+        $statusToExport = explode(',', NetReviews::getConfigValue('status_to_export', '4'));
+        if (in_array($event->getPlacedOrder()->getStatusId(), $statusToExport, true)){
+            $netReviewsOrderQueue = new NetreviewsOrderQueue();
+            $netReviewsOrderQueue->setOrderId($event->getPlacedOrder()->getId())
+                ->setStatus(0)
+                ->save();
+        }
     }
 }
